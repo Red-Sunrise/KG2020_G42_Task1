1757921e215b57c07e82b79fe5ad8ad5c878740f
Red-Sunrise <yaroslaw_proskurin@mail.ru>
2020-09-24 22:36:29 +0300

    update Drawpanel(updated cloud drawing)

--- src/Proskurin/ja/a/DrawPanel.java
+++ src/Proskurin/ja/a/DrawPanel.java
@@ -31,11 +31,15 @@ public class DrawPanel extends JPanel {
         Home home = new Home(getWidth(), getHeight(), getWidth() / 3, 5 * getHeight() / 12, 10, window, Color.GRAY, wall);
         Sun sun = new Sun(0, 0, getWidth() / 10, getWidth() / 5, 50, Color.YELLOW);
         Cloud cloud = new Cloud(getWidth() / 4, getHeight() / 50, width, 80);
+        Cloud cloud1=new Cloud(getWidth()/4,getHeight()/2,width,80);
         background.draw(gr);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 15; i++) {
             cloud.setX(getWidth() / 4 + i * width + getWidth()/36);
+            cloud1.setX(getWidth() / 4 + i * width + getWidth()/36);
             cloud.setY(i * random.nextInt(50));
+            cloud1.setY(i * random.nextInt(50));
             cloud.draw(gr);
+            cloud1.draw(gr);
         }
         tree1.draw(gr);
         tree2.draw(gr);
